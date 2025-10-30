579e07a94f2d39a5c22a118e2ad9bafce4826639
jeongmin-yeong <lucas@howser.co.kr>
2025-10-30 16:07:05 +0900

    Initial commit

---
+++ src/main/java/com/example/auto_coin_trading/AutoCoinTradingApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_coin_trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCoinTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCoinTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auto_coin_trading/AutoCoinTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_coin_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCoinTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
